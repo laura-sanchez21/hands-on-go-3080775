f8571bab4441668f677a7ae68bb950ff99305ff1
Laura Sanchez <slaura2048@gmail.com>
2025-02-04 00:52:20 -0800

    Challenge 1: importing proverb package and printing randome proverb.

--- challenges/packages/begin/main.go
+++ challenges/packages/begin/main.go
@@ -2,9 +2,18 @@
 package main
 
 // import the proverbs package
+import (
+	"fmt"
+
+	"github.com/jboursiquot/go-proverbs"
+)
 
 // getRandomProverb returns a random proverb from the proverbs package
+func getRandomeProverb() string {
+	return proverbs.Random().Saying
+}
 
 func main() {
 	// print the result of calling your getRandomProverb function
+	fmt.Println(getRandomeProverb())
 }

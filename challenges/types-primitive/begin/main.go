d9747e8fc9b3ba47442126270d6965aac0a80bb8
Laura Sanchez <slaura2048@gmail.com>
2025-02-05 04:53:22 -0800

    Challlenge 2 Soluition 1st run through

--- challenges/types-primitive/begin/main.go
+++ challenges/types-primitive/begin/main.go
@@ -1,18 +1,39 @@
 // challenges/types-primitive/begin/main.go
 package main
 
+import (
+	"fmt"
+)
+
 // use type inference to create a package-level variable "val" and assign it a value of "global"
+var val string = "temp"
 
 func main() {
 	// create a local variable "val" and assign it an int value
-
+	val := 100
 	// print the value of the local variable "val"
-
+	fmt.Printf("%v, %T\n",val, val)
 	// print the value of the package-level variable "val"
-
+	printingGlobalVariable()
 	// update the package-level variable "val" and print it again
+	modifyingGlobalVariable("new temp")
+	printingGlobalVariable()
 
 	// print the pointer address of the local variable "val"
+	//fmt.Printf("%v", &val)
+	valAddress := &val
+	// assign a value directly to the pointer address of the local variable "val" 
+	//*(&val) = 10
+	*valAddress = 11
+	//and print the value of the local variable "val"
+	//fmt.Println("\n", val)
+	fmt.Println(val)
+}
 
-	// assign a value directly to the pointer address of the local variable "val" and print the value of the local variable "val"
+func printingGlobalVariable() {
+	fmt.Println(val)
+}
+
+func modifyingGlobalVariable(newString string) {
+	val = newString
 }

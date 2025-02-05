a4cfa6c2322e47ac0a0f62ab6a6c33420ca3a7d6
Laura Sanchez <slaura2048@gmail.com>
2025-02-05 03:21:03 -0800

    Converting one type to  another Ex. float to uint

--- types/conversion/begin/main.go
+++ types/conversion/begin/main.go
@@ -1,6 +1,15 @@
 // types/conversion/begin/main.go
 package main
 
+import "fmt"
+
 func main() {
 	// declare float and convert it to an unsigned int
+	num := 10.23
+	num2 := uint(num)
+
+	fmt.Printf("num: %v=", num)
+	fmt.Printf("num2: %v=", num2)
+
+	fmt.Printf("%v=%T, %v=%T", num, num, num2, num2)
 }

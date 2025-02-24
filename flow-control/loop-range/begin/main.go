9711ddc59954074cf58cd317486d3069d8ae0366
Laura Sanchez <slaura2048@gmail.com>
2025-02-24 03:24:47 -0800

    Learning about loops in go

--- flow-control/loop-range/begin/main.go
+++ flow-control/loop-range/begin/main.go
@@ -1,16 +1,28 @@
 // flow-control/loop-range/begin/main.go
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	// initialize a slice of ints
-	//
-
+	numlist := []int{1, 2, 3, 4}
+	
 	// use for-range to iterate over the slice and print each value
-	//
+	for i, nums := range numlist {
+		fmt.Println("index:", i, " Value:", nums)
+	}
 
 	// declare a map of strings to ints
-	//
-
+	 stringToInt := map[string]int {
+		"one": 1, 
+		"two": 2,
+		"three": 3,
+	}
+	
 	// use for-range to iterate over the map and print each key/value pair
-	//
+	for i, value := range stringToInt {
+		fmt.Println("index:", i, "Value:", value)
+	}
 }
--- flow-control/loop-while/begin/main.go
+++ flow-control/loop-while/begin/main.go
@@ -1,10 +1,22 @@
 // flow-control/loop-while/begin/main.go
 package main
 
+import (
+	"fmt"
+//"strconv"
+)
+
 func main() {
 	// initialize a variable to check if the loop should continue
 	//
+	value := 5
 
 	// iterate while the condition is true
-	//
+	for value >= 0 {
+
+		//fmt.Printf("%v\n", strconv.Itoa(value))
+		fmt.Println(value)
+		value--
+	}
+	 
 }

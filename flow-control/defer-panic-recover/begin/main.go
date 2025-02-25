c836e72c7861ae39a2f63c2cedc21b75245483ae
Laura Sanchez <slaura2048@gmail.com>
2025-02-24 21:27:08 -0800

    Learning about differ, recovery, and panic in go

--- flow-control/defer-panic-recover/begin/main.go
+++ flow-control/defer-panic-recover/begin/main.go
@@ -9,8 +9,39 @@ func cleanup(msg string) {
 
 func main() {
 	// defer function calls
+	//panic("something bad happened")
+	defer cleanup("first")
+	defer cleanup("second")
 
+	fmt.Println("working in main...")
 	// defer recovery
-
+	defer func(){
+		if err := recover(); err != nil {
+			fmt.Println("recovered from panic:", err)
+		}
+	}()
 	// panic
+	panic("something bad happened")
 }
+
+// func safeDivide(a, b int) {
+// 	// Recovering from panic
+// 	defer func() {
+// 		fmt.Println("defer func")
+// 		if r := recover(); r != nil {
+// 			fmt.Println("Recovered from error:", r)
+// 		}
+// 	}()
+
+// 	if b == 0 {
+// 		fmt.Println("b == 0\n")
+// 		panic("Cannot divide by zero!") // Triggers panic
+// 	}
+// 	fmt.Println("Result:", a/b)
+// }
+
+// func main() {
+// 	//safeDivide(10, 2) // Works fine
+// 	safeDivide(10, 0) // Panics but gets recovered
+// 	fmt.Println("Program continues running...")
+// }

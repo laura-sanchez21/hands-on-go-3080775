7fc1dd74e8099c9ace5873bba1d6a9a918792ab6
Laura Sanchez <slaura2048@gmail.com>
2025-03-20 00:24:53 -0700

    Completed generic challenge and learned about creating generic functions

--- challenges/generics/begin/main.go
+++ challenges/generics/begin/main.go
@@ -1,10 +1,15 @@
 // challenges/generics/begin/main.go
 package main
 
-import "fmt"
+import (
+"fmt" 
+"golang.org/x/exp/constraints"
+)
 
 // Part 1: print function refactoring
-
+func printAny[T any](output T){
+	fmt.Println(output)
+}
 // non-generic print functions
 
 func printString(s string) { fmt.Println(s) }
@@ -14,6 +19,17 @@ func printInt(i int) { fmt.Println(i) }
 func printBool(b bool) { fmt.Println(b) }
 
 // Part 2 sum function refactoring
+type numeric interface {
+	constraints.Float | constraints.Integer
+}
+
+func sumAny[T numeric](nums ...T) T {
+	var sum T
+	for _, num := range nums {
+		sum += num
+	}
+	return sum
+}
 
 // sum sums a slice of any type
 func sum(numbers []interface{}) interface{} {
@@ -33,14 +49,19 @@ func sum(numbers []interface{}) interface{} {
 
 func main() {
 	// call non-generic print functions
-	printString("Hello")
-	printInt(42)
-	printBool(true)
+	// printString("Hello")
+	// printInt(42)
+	// printBool(true)
 
 	// call generic printAny function for each value above
+	printAny("Hello")
+	printAny(42)
+	printAny(true)
 
 	// call sum function
-	fmt.Println("result", sum([]interface{}{1, 2, 3}))
+	//fmt.Println("result", sum([]interface{}{1, 2.1, 3}))
 
 	// call generics sumAny function
+	//list := []int {1, 2, 3}
+	fmt.Println("result", sumAny(1, 2.9, 3))
 }
--- go.mod
+++ go.mod
@@ -1,9 +1,11 @@
 module github.com/LinkedInLearning/hands-on-go-3080775
 
-go 1.18
+go 1.23.0
+
+toolchain go1.23.5
 
 require (
 	github.com/davecgh/go-spew v1.1.1
 	github.com/jboursiquot/go-proverbs v0.0.2
-	golang.org/x/exp v0.0.0-20220921164117-439092de6870
+	golang.org/x/exp v0.0.0-20250305212735-054e65f0b394
 )
